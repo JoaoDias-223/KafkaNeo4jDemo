49973185155da8abad59c3c992cf09c0bf923783
João Vitor Dias <joao.santos@creditas.com>
2021-09-13 08:41:09 -0300

    chore(): initial commit
    Signed-off-by: João Vitor Dias <joao.santos@creditas.com>

---
+++ src/main/kotlin/com/kfkn4j/KafkaNeo4jDemo/KafkaNeo4jDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.kfkn4j.KafkaNeo4jDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaNeo4jDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaNeo4jDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/kfkn4j/KafkaNeo4jDemo/KafkaNeo4jDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kfkn4j.KafkaNeo4jDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaNeo4jDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
